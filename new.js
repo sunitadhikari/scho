6526a2fc72f2b95a9b6f8dea106e7131aa4270e3
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-16 23:01:02 +0545

    jjk

--- new.js
+++ new.js
@@ -3,4 +3,4 @@ const person ={
     age:50,
     eyecolor:"brown"
 };
-document.getElementById("demo").innerHTML = person["name"] + " is " +person["age"] +  " years old.";
+document.getElementById("demo").innerHTML = person["n + " is " +person["age"] +  " years old.";
