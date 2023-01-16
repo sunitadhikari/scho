4fc277f5d8167bbd912c43c2f6af549f629549ba
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-16 23:03:50 +0545

    gkjl

--- new.js
+++ new.js
@@ -2,5 +2,4 @@ const person ={
     name:"Suni",
     age:50,
     eyecolor:"brown"
-};
-document.getElementById("demonnerHTML = perso["n + " is "erson["age"] +  " years old
+};+ " is "erson["age"] +  " years old
