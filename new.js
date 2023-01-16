5062d062f3e6d2f53e1ebdfb71998358c5b95fe8
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-16 23:01:31 +0545

    gkk

--- new.js
+++ new.js
@@ -3,4 +3,4 @@ const person ={
     age:50,
     eyecolor:"brown"
 };
-document.getElementById("demo").innerHTML = person["n + " is " +person["age"] +  " years old.";
+document.getElementById("demonnerHTML = person["n + " is " +person["age"] +  " years old.";
