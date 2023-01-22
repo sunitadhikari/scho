61310ad1720f1e9ca0fe2a2042f60d9ddbe18bad
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-22 10:56:13 +0545

    JGkj

--- new.js
+++ new.js
@@ -7,7 +7,7 @@
 // //   getData();
 
 //   async function example() {
-//     // const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+//     
 //     const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
 //    const data= await response.json();
 //    console.log(data);
