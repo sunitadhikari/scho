4f2f996e4ba8bda89944d0acb3b1298fb92d14d2
sunitadhikari <sunitaadhikari2001@gmail.com>
2023-01-22 10:55:14 +0545

    Jgkj

--- new.js
+++ new.js
@@ -1,1 +1,26 @@
-cvkjdvhjkgdsfr
+// // async function getData() {
+// //     const res = await axios.get(`https://pokeapi.co/api/v2/pokemon`);
+// //     console.log(res.data.name);
+// //     setName(res.data.name);
+// //     setmoves(res.data.moves.length);
+// //   }
+// //   getData();
+
+//   async function example() {
+//     // const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+//     const response = await fetch('https://jsonplaceholder.typicode.com/todos/1');
+//    const data= await response.json();
+//    console.log(data);
+   
+
+//     // const data = await response.json();
+//     // console.log(data);
+//   }
+//   example();
+async function example{
+  const myfb = await axiox.get(`https://www.facebook.com/kanchi.adhikary`);
+  console.log(myfb.data.name);
+  // setName(myfb.data.name);
+  // setmoves(myfb.data.moves.length);
+}
+example();
